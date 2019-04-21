9e343d6d4cb5f140c58fd8981bb1a189c3e4b479
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-20 18:12:14 -0700

    add given files to templates folder

---
+++ templates/password.php
@@ -0,0 +1,16 @@
+<?php include "../inc/header.php"; ?>
+
+        <section>
+          <div class="container">
+            <div class="password">
+              <form>
+                <label for="password">Enter Password</label>
+                <input type="password" name="password"><br>
+                <input type="submit" value="Submit" class="button">
+                <a href="#" class="button button-secondary">Cancel</a>
+              </form>
+            </div>
+          </div>
+        </section>
+
+<?php include "../inc/footer.php"; ?>
