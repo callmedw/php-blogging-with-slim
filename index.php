9c82c2abf790f2e492a3b7d7d4ae0ffecb78315b
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-20 18:29:01 -0700

    update index.php for development

--- index.php
+++ index.php
@@ -1,19 +1,2 @@
 <?php
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-
-require 'vendor/autoload.php';
-
-$app = new \Slim\App;
-$app->get('index.php', function (Request $request, Response $response, array $args) {
-  $response->getBody();
-  return $response;
-});
-
-// $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-//     $name = r
-//     $response->getBody()->write("Hello, $name");
-//
-//     return $response;
-// });
-$app->run();
+include "public/index.php";
