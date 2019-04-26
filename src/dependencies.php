5ec4edb25d04af3160a9e0a1a3d65a7374f7da78
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-25 18:11:12 -0700

    add database settings

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,20 +3,47 @@
 use Slim\App;
 
 return function (App $app) {
-    $container = $app->getContainer();
+  $container = $app->getContainer();
 
-    // view renderer
-    $container['view'] = function ($c) {
-        $settings = $c->get('settings')['renderer'];
-        return new \Slim\Views\PhpRenderer($settings['template_path']);
-    };
+  // view renderer
+  $container['view'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    return new \Slim\Views\PhpRenderer($settings['template_path']);
+  };
 
-    // monolog
-    $container['logger'] = function ($c) {
-        $settings = $c->get('settings')['logger'];
-        $logger = new \Monolog\Logger($settings['name']);
-        $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-        $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-        return $logger;
+  // monolog
+  $container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new \Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    return $logger;
+  };
+
+  $container['db'] = function($c) {
+    $db = $c->get('settings')['db'];
+    $pdo = new PDO($db['dsn'].':'.$db['database']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+  };
+
+  $container['comment'] = function($c) {
+    return new App\Model\Comment($c->get('db'));
+  };
+
+  $container['entry'] = function($c) {
+    return new App\Model\Entry($c->get('db'));
+  };
+
+  $container['errorHandler'] = function ($c) {
+    return function ($request, $response, $exception) use ($c) {
+    $data = [
+      'status' => 'error',
+      'code' => $exception->getCode(),
+      'message' => $exception->getMessage(),
+    ];
+    return $response->withJson($data, $exception->getCode());
     };
+  };
 };
--- src/settings.php
+++ src/settings.php
@@ -15,5 +15,11 @@ return [
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
         ],
+
+        // Database
+        'db' => [
+            'dsn' => 'sqlite',
+            'database' => __DIR__.'/database/blog.db',
+      ],
     ],
 ];
