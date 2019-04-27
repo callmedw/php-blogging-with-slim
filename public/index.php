c7d0ff6b2568612cf3a373e1423ef92ef70e236d
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-27 14:03:51 -0700

    repop pub/index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,36 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+
+// Twig loader
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates/');
 $twig = new \Twig\Environment($loader);
-echo $twig->render('index.twig', ['name' => 'Dana']);
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
+$app->run();
