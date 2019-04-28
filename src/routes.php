9762f3d6c5e4ed64f69ad778fe8a50b427028d63
Dana Weiss <mynameisdanaweiss@gmail.com>
2019-04-28 14:50:59 -0700

    get entries functioning

--- src/routes.php
+++ src/routes.php
@@ -3,25 +3,29 @@ use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-return function (App $app, $twig) {
+return function (App $app) {
   $container = $app->getContainer();
   $database = $container->get('db');
 
   $app->get('/', function (Request $request, Response $response, array $args) use ($container, $database) {
-    $container->get('logger')->info("Slim-Skeleton '/' route");
     $args['entries'] = $this->entry->getEntryList($database);
     return $this->view->render($response, 'index.twig', $args);
   });
 
+  $app->get('/entries/{id}/edit', function (Request $request, Response $response, array $args) use ($container, $database) {
+    $entry_id = $request->getAttribute('id');
+    $args['entry'] = $this->entry->getEntry($database, $entry_id);
+    return $this->view->render($response, 'edit.twig', $args);
+    }
+  );
+
   $app->get('/entries/{id}', function (Request $request, Response $response, array $args) use ($container, $database) {
     $entry_id = $request->getAttribute('id');
-    $container->get('logger')->info("Slim-Skeleton '/entries/{$entry_id}' route");
     $args['entry'] = $this->entry->getEntry($database, $entry_id);
     return $this->view->render($response, 'detail.twig', $args);
   });
 
-  $app->get('/new', function (Request $request, Response $response, array $args) use ($container) {
-    $container->get('logger')->info("Slim-Skeleton '/new' route");
+  $app->get('entries/new', function (Request $request, Response $response, array $args) use ($container, $database) {
     return $this->view->render($response, 'new.twig', $args);
     }
   );
